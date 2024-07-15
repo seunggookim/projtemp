8d34a66c539fbb70011542660da725976eb1a4af
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-07-15 09:05:36 +0200

    README-update

--- create_project.sh
+++ create_project.sh
@@ -16,16 +16,16 @@ cat << EOF > README.md
 ## directory structure
 
 ### On respo (synchronized)
-\`./code\`  for functions, scripts, small (<10 MB) packages for each language
+\`./code\`  for functions, scripts, small (<50 MB) packages for each language
 
 \`./docs\`  for documents (notes, presentation slides, manuscripts, other human-readable files)
 
 \`./figs\`  to dump figure files in
 
-\`./meta\`  for small (<10 MB) meta-data files
+\`./meta\`  for small (<50 MB) meta-data files
 
 ### On local (not synchronized)
-\`./data\`  for large (>10 MB) files. \
+\`./data\`  for large (>50 MB) files. \
 The folder name "data" is set NOT TO SYNCHRONIZED (see \`.gitignore\`)
 EOF
 rm create_project.sh
