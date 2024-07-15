9d084d460a4174a3493955329ac06f89b15d703f
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-07-15 09:03:13 +0200

    README-FIXED

--- create_project.sh
+++ create_project.sh
@@ -11,21 +11,22 @@ gh repo create ${ProjName} --template projtemp --private
 gh repo clone ${ProjName}
 cd ${ProjName}
 cat << EOF > README.md
-# ${ProjName}\
+# ${ProjName}
 
 ## directory structure
 
 ### On respo (synchronized)
-`./code`  for functions, scripts, small (<10 MB) packages for each language
+\`./code\`  for functions, scripts, small (<10 MB) packages for each language
 
-`./docs`  for documents (notes, presentation slides, manuscripts, other human-readable files)
+\`./docs\`  for documents (notes, presentation slides, manuscripts, other human-readable files)
 
-`./figs`  to dump figure files in
+\`./figs\`  to dump figure files in
 
-`./meta`  for small (<10 MB) meta-data files
+\`./meta\`  for small (<10 MB) meta-data files
 
 ### On local (not synchronized)
-`./data`  for large (>10 MB) files. The folder name "data" is set NOT TO SYNCHRONIZED (see `.gitignore`)
+\`./data\`  for large (>10 MB) files. \
+The folder name "data" is set NOT TO SYNCHRONIZED (see \`.gitignore\`)
 EOF
 rm create_project.sh
 git add .
