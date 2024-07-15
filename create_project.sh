8acb81bafa495bb1f87730bdc9ab31ddf7aab5d0
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-07-15 08:59:30 +0200

    README-dir-struct

--- create_project.sh
+++ create_project.sh
@@ -10,7 +10,23 @@ ProjName=${1}
 gh repo create ${ProjName} --template projtemp --private
 gh repo clone ${ProjName}
 cd ${ProjName}
-echo "# ${ProjName}" > README.md
+cat << EOF > README.md
+# ${ProjName}\
+
+## directory structure
+
+### On respo (synchronized)
+`./code`  for functions, scripts, small (<10 MB) packages for each language
+
+`./docs`  for documents (notes, presentation slides, manuscripts, other human-readable files)
+
+`./figs`  to dump figure files in
+
+`./meta`  for small (<10 MB) meta-data files
+
+### On local (not synchronized)
+`./data`  for large (>10 MB) files. The folder name "data" is set NOT TO SYNCHRONIZED (see `.gitignore`)
+EOF
 rm create_project.sh
 git add .
 git commit -m "Initial commit"
