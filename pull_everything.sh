f1c042d464402c024b0491f53223cc5c01d6b2cf
Seung-Goo KIM <dr.seunggoo.kim@gmail.com>
2024-09-17 19:01:28 +0200

    pull+

--- pull_everything.sh
+++ pull_everything.sh
@@ -2,8 +2,10 @@
 #find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin main \;
 for DN in `find . -maxdepth 1 -type d`
 do
-	git --git-dir=${DN}/.git --work-tree=${DN} pull origin master
-  sleep 3
-	git --git-dir=${DN}/.git --work-tree=${DN} pull origin main
+#	git --git-dir=${DN}/.git --work-tree=${DN} pull origin master
+#  sleep 3
+#	git --git-dir=${DN}/.git --work-tree=${DN} pull origin main
+#  sleep 3
+	git --git-dir=${DN}/.git --work-tree=${DN} pull
   sleep 3
 done
