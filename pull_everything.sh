c5fe6f011df6f9887907b675136ae6d07664e694
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-09-20 12:23:43 +0200

    better-pull

--- pull_everything.sh
+++ pull_everything.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -x
 #find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin main \;
-for DN in `find . -maxdepth 1 -type d`
+HERE=$(pwd)
+for DN in `find ${HERE}/ -maxdepth 1 -type d`
 do
-#	git --git-dir=${DN}/.git --work-tree=${DN} pull origin master
-#  sleep 3
-#	git --git-dir=${DN}/.git --work-tree=${DN} pull origin main
-#  sleep 3
-	git --git-dir=${DN}/.git --work-tree=${DN} pull
-  sleep 3
+#  if [[ uname -eq "Linux" ]]; then
+#    git --git-dir=${DN}/.git --work-tree=${DN} pull origin master
+#    sleep 3
+#    git --git-dir=${DN}/.git --work-tree=${DN} pull origin main
+#    sleep 3
+#  else
+#    git --git-dir=${DN}/.git --work-tree=${DN} pull
+  if [ -d ${DN}/.git ]
+  then
+    cd ${DN}
+    git pull
+    sleep 3
+  fi
 done
+cd ${HERE}
+
