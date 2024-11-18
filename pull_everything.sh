d191ec63140d0d44b02b7088b8ae4f629d839492
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-11-18 17:45:11 +0100

    .

--- pull_everything.sh
+++ pull_everything.sh
@@ -1,15 +1,7 @@
 #!/bin/bash -x
-#find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin main \;
 HERE=$(pwd)
 for DN in `find ${HERE}/ -maxdepth 1 -type d`
 do
-#  if [[ uname -eq "Linux" ]]; then
-#    git --git-dir=${DN}/.git --work-tree=${DN} pull origin master
-#    sleep 3
-#    git --git-dir=${DN}/.git --work-tree=${DN} pull origin main
-#    sleep 3
-#  else
-#    git --git-dir=${DN}/.git --work-tree=${DN} pull
   if [ -d ${DN}/.git ]
   then
     cd ${DN}
