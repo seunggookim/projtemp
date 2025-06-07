58a085d1495bb4d4f0278bc1eba0899a7a61e546
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2025-06-07 07:36:51 +0200

    reverted

---
+++ pull_everything.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+HERE=$(pwd)
+for DN in `find ${HERE}/ -maxdepth 1 -type d`
+do
+  if [ -d ${DN}/.git ]
+  then
+    cd ${DN}
+    git pull --recurse
+    sleep 3
+  fi
+done
+cd ${HERE}
+
