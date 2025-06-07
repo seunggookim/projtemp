f8d3c0345f285b13ea42d4f86a805ae5c25d81bf
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2025-06-07 07:42:11 +0200

    ncml-code+

--- create_project.sh
+++ create_project.sh
@@ -22,7 +22,7 @@ fi
 ProjName=${1}
 gh repo create ${ProjName} --template projtemp --private
 sleep 3 # give a few seconds to github to create a new one
-gh repo clone ${ProjName} --recursive
+gh repo clone ${ProjName} --recurse-submodules
 cd ${ProjName}
 cat << EOF > README.md
 # ${ProjName}
